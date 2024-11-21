4dc5fff334159753ddbb83e11d219a64743da8c5
Josh <josh.letcher@skyspecs.com>
2024-11-21 15:35:22 -0600

    idk what changed

--- init.lua
+++ init.lua
@@ -373,7 +373,7 @@ require('lazy').setup({
       --  - Insert mode: <c-/>
       --  - Normal mode: ?
       --
-    -- This opens a window that shows you all of the keymaps for the current
+      -- This opens a window that shows you all of the keymaps for the current
       -- Telescope picker. This is really useful to discover what Telescope can
       -- do as well as how to actually do it!
 
@@ -410,7 +410,11 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+      vim.keymap.set('n', '<leader>sg', function()
+        builtin.live_grep {
+          glob_pattern = { '!node_modules', '!.git', '!yarn.lock' },
+        }
+      end, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
